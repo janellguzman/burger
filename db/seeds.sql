d4227e1b85bce5185cb5e1ebd5b7d1cda6e827a2
Janell Guzman <janellguzman@gmail.com>
2018-06-05 19:01:02 -0700

    Adding DB info.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bohemian Radishy", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eggers Can't Be Cheesers", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hit Me With Your Best Shallot", true);
